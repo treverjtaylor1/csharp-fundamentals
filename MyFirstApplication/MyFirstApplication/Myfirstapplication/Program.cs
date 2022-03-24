a8927e83dea5296699eaa4347f30baee1c78ff77
Trever Taylor <treverjtaylor1@gmail.com>
2022-03-23 21:14:44 -0500

    CAC 103 My First Exercise Lab

--- MyFirstApplication/MyFirstApplication/Myfirstapplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Myfirstapplication/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Myfirstapplication;
+using Myfirstapplication.Lessons;
 
-
+// The Below Statement calls a local function
 HouseExample();
+
+/*
+multi line comment.
+ */
 
 void HouseExample()
 {
@@ -9,6 +14,29 @@ House myHouse = new House();
 myHouse.DoorOpenClose();
 }
 
+/* This local function is 
+ * for my first exercise lab
+ */
 
+MyLocalFunction();
+
+void MyLocalFunction()
+{
+    Console.WriteLine("This is my first sentence.");
+    Console.WriteLine("I am new to CSharp");
+    Console.WriteLine("This CSharp course is c0ol.");
+    Console.WriteLine("I am learning stuff everyday.");
+}
+
+void Lesson1Example()
+{
+    lesson1 myLesson = new lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongfromInt();
+
+
+}
 
 
